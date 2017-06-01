c06a2b82f446cffdbba007fe7380d213c84c539a
kxhuan <kxhuan@dolby.com>
2017-06-02 09:17:59 +1000

    use script directory instead of hard-coded paths for drive mapping

--- start-docker.sh
+++ start-docker.sh
@@ -1,1 +1,4 @@
-docker run -it --rm  -p 8888:8888 -v /Users/kxhuan/Documents/code/github/cs231n:/root/cs231n cs231n-docker sh -c "jupyter notebook --ip=0.0.0.0 --no-browser"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+docker run -it --rm  -p 8888:8888 -v ${DIR}:/root/cs231n cs231n-docker sh -c "jupyter notebook --ip=0.0.0.0 --no-browser"
+
